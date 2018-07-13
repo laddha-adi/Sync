e5bc589716594a3d3ae614075d0d4adafda95d31
laddha-adi <f2016038@pilani.bits-pilani.ac.in>
2018-07-13 09:22:45 +0530

    latest code

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,38 +3,21 @@ import org.opendatakit.sync.client.SyncClient;
 
 import java.net.URL;
 
+import org.apache.wink.json4j.JSONException;
+import org.opendatakit.sync.client.SyncClient;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class main {
-    String agg_url;
-    String appId;
-    String host;
-    String userName;
-    String password;
-    String version;
+    public static void main(String[] args) throws IOException, JSONException {
+        SyncClient syncClient = new SyncClient();
 
-    public static void main(String[] args){
-        main s = new main();
-        try {
-            s.setUp();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+        syncClient.init("ec2-52-25-205-39.us-west-2.compute.amazonaws.com", "bsmith", "pass");
 
-    private void setUp() throws Exception {
-        agg_url = "http://ec2-52-25-205-39.us-west-2.compute.amazonaws.com";
-        appId = "default";
+        ArrayList<Map<String, Object>> users = syncClient.getUsers("http://ec2-52-25-205-39.us-west-2.compute.amazonaws.com/odktables", "default");
 
-        userName = "bsmith";
-        password = "pass";
-
-        URL url = new URL(agg_url);
-        host = url.getHost();
-
-        SyncClient wc = new SyncClient();
-        wc.init(host, userName, password);
-
-       // Thread.sleep(10000);
-        wc.getUsers(agg_url,appId);
-        wc.close();
+        System.out.println(users);
     }
 }
