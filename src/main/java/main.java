0f1b901e89d239d72c8dbe7c0760c6b2a9cd8101
laddha-adi <f2016038@pilani.bits-pilani.ac.in>
2018-07-19 12:44:20 +0530

    Sync-Client

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,12 +12,12 @@ import java.util.Map;
 
 public class main {
     public static void main(String[] args) throws IOException, JSONException {
+
+        String username = "";
+        String password = "";
         SyncClient syncClient = new SyncClient();
-
-        syncClient.init("ec2-52-25-205-39.us-west-2.compute.amazonaws.com", "bsmith", "pass");
-
-        ArrayList<Map<String, Object>> users = syncClient.getUsers("http://ec2-52-25-205-39.us-west-2.compute.amazonaws.com/odktables", "default");
-
+        syncClient.init("http://ec2-18-236-164-13.us-west-2.compute.amazonaws.com", username, password);
+        ArrayList<Map<String, Object>> users = syncClient.getUsers("http://ec2-18-236-164-13.us-west-2.compute.amazonaws.com/odktables", "default");
         System.out.println(users);
     }
 }
